d626a3ffe9d240ab0dfab2610d1eebe5ed5a9685
rrsingh11 <rrsingh1101@gmail.com>
2023-08-22 10:48:18 +0530

    funcs added

--- deck.go
+++ deck.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+	"math/rand"
+)
 
 // Create a new deck as a slice of strings
 type deck []string
@@ -26,4 +32,45 @@ func newDeck() deck {
 	}
 
 	return cards
+}
+
+// Convert []string to a single string sep by ","
+func (d deck) toString() string{
+	return strings.Join([]string(d), ",")
+} 
+
+// Save to File
+func (d deck) saveToFile() {
+	err := os.WriteFile("myDeck", []byte(d.toString()), 0666)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}	
+}
+
+// Get deck from file
+func getDeckFromFile(file string) deck {
+	bs, err := os.ReadFile(file)
+	if(err != nil) {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	return deck(strings.Split(string(bs), ","))
+}
+
+// Shuffle deck
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano()) // Each time convert it into int64
+	r := rand.New(source) // create new seed
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+}
+
+// Create a hand
+func deal(d deck, num int) (deck, deck) {
+	return d[:num], d[num:]
 }
--- main.go
+++ main.go
@@ -4,6 +4,12 @@ package main
 
 func main() {
 	cards := newDeck()
+	// cards := getDeckFromFile()
+	cards.shuffle()
+
+	// hand, remainingCards := deal(cards, 5)
 
 	cards.print()
+	// hand.print()
+	// cards.saveToFile()
 }
